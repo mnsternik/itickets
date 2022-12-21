9ab7083b6c81a48588aed66e49e9617e616301dd
Marcin Sternik <mnsternik@gmail.com>
2022-12-21 02:26:19 +0100

    handling error codes fixed

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,61 @@
 import { ref, set, onValue, push, child } from 'firebase/database';
-import { db } from '../util/firebase';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { db, auth } from '../util/firebase';
 
 //add check if data exists (for example 2 groups with same name? 
+
+export function writeNewUserData(userData, password, setError) {
+    createUserWithEmailAndPassword(auth, userData.email, password)
+        .then((userCredential) => {
+            const uid = userCredential.user.uid;
+            const newUserRef = ref(db, '/users/' + uid);
+            const updatedUserData = { ...userData, uid: uid };
+            set(newUserRef, updatedUserData);
+        })
+        .catch((error) => {
+            if (error.code === 'auth/email-already-in-use') {
+                setError('E-mail already in use.')
+            } else if (error.code === 'auth/weak-password') {
+                setError('Password must be at least 6 characters long')
+            } else {
+                setError('Authentication error. Try again later. ');
+            }
+        });
+};
+
+export function signUserOut() {
+    signOut(auth)
+};
+
+export function signInUser(email, password, setError) {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            return userCredential.user.uid;
+        })
+        .catch((error) => {
+            console.log(error.code);
+            setError("Incorrect login data.");
+        });
+};
+
+export function readUserData(uid, updateUser) {
+    const userRef = ref(db, '/users/' + uid);
+    onValue(userRef, (snapshot) => {
+        const userData = snapshot.val();
+        console.log(userData);
+        updateUser(userData);
+    });
+};
+
+export function readAllUsersData(updateUsers) {
+    const usersRef = ref(db, 'users');
+    onValue(usersRef, (snapshot) => {
+        const usersData = snapshot.val();
+        console.log(usersData);
+        updateUsers(usersData);
+    });
+};
+
 
 export function writeNewResponseData(taskId, responseData) {
     const newResponseRef = push(child(ref(db), '/responses/' + taskId));
@@ -15,11 +69,6 @@ export function writeNewTaskData(taskData) {
     const newTaskRef = ref(db, '/tasks/' + taskData.id);
     set(newTaskRef, taskData);
 };
-
-/*export function writeNewTaskId() {
-    const newTaskIdRef = ref(db, 'id');
-}*/
-
 
 
 export function writeNewGroupData(groupData) {
@@ -35,7 +84,6 @@ export function writeNewGroupMember(groupName, userId) {
         .join('');
 
     const newMemberRef = ref(db, '/groups/' + groupId + '/members');
-
     push(newMemberRef, userId);
 };
 
@@ -57,7 +105,6 @@ export function readAllTasksData(updateTasks) {
     return onValue(tasksRef, (snapshot) => {
         const fetchedTasks = snapshot.val();
         const transformedTasks = [];
-
         for (const taskKey in fetchedTasks) {
             transformedTasks.push(fetchedTasks[taskKey])
         }
@@ -81,7 +128,6 @@ export function readResponseData(taskId, updateResponses) {
     return onValue(responsesRef, (snapshot) => {
         const fetchedResponses = snapshot.val()
         const transformedResponses = [];
-
         for (const resKey in fetchedResponses) {
             transformedResponses.push(fetchedResponses[resKey])
         }
@@ -96,7 +142,6 @@ export function readCategoriesData(updateCategories) {
     onValue(categoriesRef, (snapshot) => {
         const categories = snapshot.val();
         const transformedCategories = [];
-
         for (let categoryKey in categories) {
             transformedCategories.push(categories[categoryKey]);
         }
@@ -112,7 +157,6 @@ export function readGroupsData(updateGroups) {
     onValue(groupsRef, (snapshot) => {
         const groups = snapshot.val();
         const transformedGroups = [];
-
         for (let groupKey in groups) {
             transformedGroups.push(groups[groupKey]);
         }
