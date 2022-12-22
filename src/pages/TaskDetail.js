353a85afa1e60acb30a55c4e3ca1c1ac3c895802
Marcin Sternik <mnsternik@gmail.com>
2022-12-22 02:47:59 +0100

    Handling no task finded added

--- src/pages/TaskDetail.js
+++ src/pages/TaskDetail.js
@@ -1,21 +1,21 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { readSingleTaskData, readGroupsData, readCategoriesData, readAllUsersData } from '../lib/api';
+import { readSingleTaskData, readAllGroupsData, readCategoriesData, readAllUsersData } from '../lib/api';
 
 import TaskDetailForm from '../components/TaskDetail/TaskDetailForm';
 import NewResponse from '../components/Response/NewResponse';
 import ResponsesList from '../components/Response/ResponsesList';
 import TaskDetailActions from '../components/TaskDetail/TaskDetailActions';
 
-import { Divider, Paper } from '@mui/material';
+import { Divider, Paper, Typography } from '@mui/material';
 
 const TaskDetail = () => {
 
-    //add notaskfound component or smth
     const params = useParams();
     const { taskId } = params;
 
     const [isFormDisabled, setIsFormDisabled] = useState(true);
+
     const [groups, setGroups] = useState([]);
     const [categories, setCategories] = useState([]);
     const [users, setUsers] = useState([]);
@@ -32,30 +32,49 @@ const TaskDetail = () => {
         modificationDate: '',
     });
 
+
     const groupsNamesArr = groups.map(group => group.name);
     const categoriesNamesArr = categories.map(category => category.name);
 
+    let selectedGroupMembers = [];
+    if (taskData && taskData.currentGroup) {
+        selectedGroupMembers = users.filter(user => user.group === taskData.currentGroup);
+    }
+    const groupMembersSelectOptions = selectedGroupMembers.map(user => ({ name: user.name, value: user.uid }));
+
     useEffect(() => {
         readSingleTaskData(taskId, setTaskData);
-        readGroupsData(setGroups);
+        readAllGroupsData(setGroups);
         readCategoriesData(setCategories);
         readAllUsersData(setUsers);
     }, [taskId])
 
-    const priorityChangeHandler = (event) => {
-        setTaskData({ ...taskData, priority: event.target.value });
-    };
 
     const currentUserChangeHandler = (event) => {
-        setTaskData({ ...taskData, currentUser: event.target.value });
+        const selectedUser = users.find(user => user.uid === event.target.value);
+        setTaskData({
+            ...taskData,
+            currentUserId: selectedUser.uid,
+            currentUser: selectedUser.name,
+            currentGroup: selectedUser.group
+        });
     };
 
     const currentGroupChangeHandler = (event) => {
-        setTaskData({ ...taskData, currentGroup: event.target.value });
+        setTaskData({
+            ...taskData,
+            currentGroup: event.target.value,
+            currentUser: '',
+            currentUserId: ''
+        });
     };
 
     const categoryChangeHandler = (event) => {
         setTaskData({ ...taskData, category: event.target.value });
+    };
+
+    const priorityChangeHandler = (event) => {
+        setTaskData({ ...taskData, priority: event.target.value });
     };
 
     const statusChangeHandler = (event) => {
@@ -71,47 +90,57 @@ const TaskDetail = () => {
     };
 
     return (
-        <Paper
-            elevation={3}
-            sx={{
-                minWidth: 700,
-                minHeight: 800,
-                my: 4,
-                p: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-around',
-            }}
-        >
-            <TaskDetailActions
-                onCurrentUserChange={currentUserChangeHandler}
-                onCurrentGroupChange={currentGroupChangeHandler}
-                onToggleFormChangeable={toggleFormChangeable}
-                onUpdate={updateTaskHandler}
-                isFormDisabled={isFormDisabled}
-                taskData={taskData}
-            />
+        <>
+            {taskData && taskData.id && taskData.id.length > 0 &&
+                <Paper
+                    elevation={3}
+                    sx={{
+                        minWidth: 700,
+                        minHeight: 800,
+                        my: 4,
+                        p: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        justifyContent: 'space-around',
+                    }}
+                >
 
-            <Divider />
+                    <TaskDetailActions
+                        onCurrentUserChange={currentUserChangeHandler}
+                        onCurrentGroupChange={currentGroupChangeHandler}
+                        onToggleFormChangeable={toggleFormChangeable}
+                        onUpdate={updateTaskHandler}
+                        isFormDisabled={isFormDisabled}
+                        taskData={taskData}
+                    />
 
-            <TaskDetailForm
-                onPriorityChange={priorityChangeHandler}
-                onCurrentUserChange={currentUserChangeHandler}
-                onCurrentGroupChange={currentGroupChangeHandler}
-                onCategoryChange={categoryChangeHandler}
-                onStatusChange={statusChangeHandler}
-                groups={groupsNamesArr}
-                categories={categoriesNamesArr}
-                taskData={taskData}
-                isFormDisabled={isFormDisabled}
+                    <Divider />
 
-            />
+                    <TaskDetailForm
+                        onPriorityChange={priorityChangeHandler}
+                        onCurrentUserChange={currentUserChangeHandler}
+                        onCurrentGroupChange={currentGroupChangeHandler}
+                        onCategoryChange={categoryChangeHandler}
+                        onStatusChange={statusChangeHandler}
+                        groups={groupsNamesArr}
+                        users={groupMembersSelectOptions}
+                        categories={categoriesNamesArr}
+                        taskData={taskData}
+                        isFormDisabled={isFormDisabled}
+                    />
 
-            <ResponsesList taskData={taskData} />
+                    <ResponsesList taskData={taskData} />
 
-            <NewResponse taskData={taskData} onTaskUpdate={updateTaskHandler} />
+                    <NewResponse taskData={taskData} onTaskUpdate={updateTaskHandler} />
+                </Paper>
+            }
 
-        </Paper>
+            {!taskData &&
+                <Typography variant='h5' sx={{ p: 4 }}>
+                    Task not found.
+                </Typography>
+            }
+        </>
     )
 };
 
