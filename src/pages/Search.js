81cd2b704300115546d0766a2c8db1fbb5d7fb59
Marcin Sternik <mnsternik@gmail.com>
2023-02-21 21:19:39 +0100

    Fetching data for filtering task added

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { readAllGroupsData, readAllUsersData, readCategoriesData } from '../lib/api';
+
+import SearchOptionsForm from '../components/Search/SearchOptionsForm';
+
 import { Box } from '@mui/system';
+
 
 const Search = () => {
 
+    const [allGroups, setAllGroups] = useState([]);
+    const [allUsers, setAllUsers] = useState([]);
+    const [categories, setCategories] = useState([]);
+
+    const priorities = useSelector(state => state.tasks.priorities);
+
+    useEffect(() => {
+        readAllGroupsData(setAllGroups);
+        readAllUsersData(setAllUsers);
+        readCategoriesData(setCategories);
+    }, []);
 
     return (
         <Box>
+            <SearchOptionsForm
+                groups={allGroups}
+                users={allUsers}
+                priorities={priorities}
+                categories={categories}
+            />
         </Box>
     );
 };
