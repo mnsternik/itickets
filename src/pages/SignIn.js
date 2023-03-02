716b8ab1dc3d6b29b75b9ed81e38db353fd4e881
Marcin Sternik <mnsternik@gmail.com>
2023-03-02 19:58:08 +0100

    Error handling corrected

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,77 +1,78 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import { signUserIn } from '../lib/api';
 
-import { Paper, TextField, Button, Typography, Divider } from "@mui/material"
+import { Stack, TextField, Button, Typography, Alert, Box } from "@mui/material"
+
+
+const initLoginState = {
+    email: '',
+    password: '',
+};
 
 const SignIn = () => {
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
+    const [loginState, dispatchLogin] = useReducer((state, action) => {
+        return { ...state, ...action }
+    }, initLoginState)
 
-    const emailChangeHandler = (event) => {
-        setEmail(event.target.value);
-    };
+    const [error, setError] = useState(false)
 
-    const passwordChangeHandler = (event) => {
-        setPassword(event.target.value);
-    };
+    const submitHandler = (event) => {
+        event.preventDefault();
 
-    const signInHandler = () => {
-        if (email.trim() === '' || password.trim() === '') {
-            setError('Incorecct login data.')
+        if (loginState.email.trim() === '' || loginState.password.trim() === '') {
+            setError('Cannot be empty')
             return;
         }
-        signUserIn(email, password, setError);
+
+        signUserIn(loginState.email, loginState.password, setError);
     };
 
     return (
-        <Paper elevation={2} sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-evenly',
-            alignItems: 'center',
-            minHeight: 440,
-            width: 600,
-            p: 2,
-            mx: 'auto',
-            mt: 6,
-        }}>
+
+        <Stack
+            spacing={2}
+            sx={{
+                width: 420,
+                mt: 12,
+                mx: 'auto',
+                textAlign: 'center'
+            }}
+        >
 
             <Typography variant="h5" sx={{ mx: 'auto', mb: 2 }}>
                 Sign in
             </Typography>
 
-            <TextField
-                label='E-mail'
-                value={email}
-                onChange={emailChangeHandler}
-                sx={{ width: 240 }}
-            />
+            <Box component='form' onSubmit={submitHandler}>
+                <TextField
+                    label='E-mail'
+                    value={loginState.email}
+                    onChange={(e) => dispatchLogin({ email: e.target.value })}
+                    sx={{ width: 240 }}
+                />
 
-            <TextField
-                label='Password'
-                value={password}
-                type="password"
-                onChange={passwordChangeHandler}
-                sx={{ width: 240 }}
-            />
+                <TextField
+                    label='Password'
+                    value={loginState.password}
+                    type="password"
+                    onChange={(e) => dispatchLogin({ password: e.target.value })}
+                    sx={{ width: 240, my: 1 }}
+                />
 
-            {error && <Typography variant="subtitle1" sx={{ color: 'error.main' }}>
-                {error}
-            </Typography>}
+                <Button
+                    variant="contained"
+                    type="submit"
+                    sx={{ width: 240 }}
+                >
+                    Login
+                </Button>
 
+                {error && <Alert severity="error" sx={{ mx: 'auto', width: 240 }}>
+                    {error}
+                </Alert>}
 
-
-            <Button
-                variant="contained"
-                onClick={signInHandler}
-                sx={{ width: 180 }}
-            >
-                Login
-            </Button>
-
-            <Divider />
+            </Box>
 
             <Typography variant="subtitle2" sx={{ fontWeight: 200 }}>
                 In case of problem contact our Helpdesk: <br />
@@ -79,7 +80,7 @@ const SignIn = () => {
                 E-Mail: <strong>example@mail.com</strong>
             </Typography>
 
-        </Paper>
+        </Stack>
     )
 };
 
