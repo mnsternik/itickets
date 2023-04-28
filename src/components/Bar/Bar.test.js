3c8151791e98a611b9e51fd6e17a53b46cfb0b1b
Marcin Sternik <mnsternik@gmail.com>
2023-04-28 17:32:32 +0200

    Test for checking logged user username added

---
+++ src/components/Bar/Bar.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import Bar from './Bar';
+
+describe('Bar component', () => {
+
+    const initialState = {
+        auth: {
+            userData: {
+                name: 'John Doe'
+            }
+        }
+    };
+
+    let mockStore;
+    beforeEach(() => {
+        mockStore = configureStore(initialState);
+    });
+
+    test('should display correct username from Redux store', () => {
+        render(
+            <Provider store={mockStore}>
+                <Bar />
+            </Provider>
+        )
+        const loggedUserInfo = screen.getByText(`Logged as ${initialState.auth.userData.name}`, { exact: false })
+        expect(loggedUserInfo).toBeInTheDocument();
+    });
+});
