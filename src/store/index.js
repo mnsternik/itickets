b1c3f97e132d73273a5b70558004e1d901c28b33
Marcin Sternik <mnsternik@gmail.com>
2023-05-12 23:20:54 +0200

    setupStore function created for test-utils

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,28 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import authReducer from './auth';
 import tasksRedcuder from './tasks';
 import uiReducer from './ui';
 
-const store = configureStore({
-    reducer: {
-        auth: authReducer,
-        tasks: tasksRedcuder,
-        ui: uiReducer,
-    }
-});
+const rootReducer = combineReducers({
+    auth: authReducer,
+    tasks: tasksRedcuder,
+    ui: uiReducer,
+})
 
-export default store; 
+// const store = configureStore({
+//     reducer: {
+//         auth: authReducer,
+//         tasks: tasksRedcuder,
+//         ui: uiReducer,
+//     }
+// });
+
+export const setupStore = preloadedState => {
+    return configureStore({
+        reducer: rootReducer,
+        preloadedState
+    })
+}
+
+//export default store; 
