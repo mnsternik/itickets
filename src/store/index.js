a60d5eab72bda805c056c6c45e883fff90746109
mnsternik <31092045+mnsternik@users.noreply.github.com>
2023-06-01 08:22:27 +0200

    Old store object deleted and replaced with setupStore function

--- src/store/index.js
+++ src/store/index.js
@@ -8,21 +8,11 @@ const rootReducer = combineReducers({
     auth: authReducer,
     tasks: tasksRedcuder,
     ui: uiReducer,
-})
-
-// const store = configureStore({
-//     reducer: {
-//         auth: authReducer,
-//         tasks: tasksRedcuder,
-//         ui: uiReducer,
-//     }
-// });
+});
 
 export const setupStore = preloadedState => {
     return configureStore({
         reducer: rootReducer,
         preloadedState
     })
-}
-
-//export default store; 
+};
