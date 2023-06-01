84f245b181b3a23730cabee25cd2cdfaaa6ad062
mnsternik <31092045+mnsternik@users.noreply.github.com>
2023-06-01 08:26:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { BrowserRouter } from "react-router-dom";
 import AuthProvider from './providers/AuthProvider';
 import { setupStore } from './store';
 import App from './App';
-
+import './index.css';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const store = setupStore();
@@ -26,5 +27,4 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+
