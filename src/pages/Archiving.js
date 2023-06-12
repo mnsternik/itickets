b2b1ea59c01a5c3f42aa4886083fb0f05588a4c3
Marcin Sternik <mnsternik@gmail.com>
2023-06-12 17:16:48 +0200

    Archiving.js created

--- src/pages/Archiving.js
+++ src/pages/Archiving.js
@@ -1,26 +1,35 @@
 import { useState, useEffect } from "react";
-import { readAllTasksData, archiveTasks } from "../lib/api";
+import { readAllTasksData, archiveTasks, readArchivedTasks } from "../lib/api";
 
-import { Typography, Paper, TextField, Button, Stack, Box } from "@mui/material";
+import { Typography, Paper, TextField, Button, Stack, Box, Alert } from "@mui/material";
 
 const Archiving = () => {
 
     const [allTasks, setAllTasks] = useState([]);
     const [archivedTasks, setArchivedTasks] = useState([]);
-    const [archTimeFrame, setArchTimeFrame] = useState(12);
+    const [archiveThresholdInDays, setArchiveThresholdInDays] = useState(365);
+    const [showAlert, setShowAlert] = useState(false);
+
+    const tasksToArchive = allTasks.filter((task) => {
+        const cutoff = Date.now() - archiveThresholdInDays * 24 * 60 * 60 * 1000; // days converted to miliseconds
+        const taskDate = new Date(task.modificationDate);
+        if (!archiveThresholdInDays.length) {
+            return false;
+        }
+        return taskDate.getTime() < cutoff;
+    });
 
     useEffect(() => {
-        readAllTasksData(setAllTasks)
+        readAllTasksData(setAllTasks);
+        readArchivedTasks(setArchivedTasks);
     }, [])
 
     const timeFrameChangeHandler = (event) => {
-        // validation...
-        setArchTimeFrame(event.target.value);
+        setArchiveThresholdInDays(event.target.value);
     };
 
     const archiveTasksHandler = () => {
-        console.log('archive start')
-        archiveTasks(archTimeFrame)
+        archiveTasks(archiveThresholdInDays)
     };
 
     return (
@@ -30,27 +39,41 @@ const Archiving = () => {
                     Archiving
                 </Typography>
 
-                <Typography variant="body2">
-                    Number of tasks in main databse: {allTasks.length}
+                <Typography variant="body1">
+                    Tasks in main: <strong>{allTasks.length}</strong>
                 </Typography>
 
-                <Typography variant="body2">
-                    Number of tasks in archive databse: {archivedTasks.length}
+                <Typography variant="body1">
+                    Tasks in main older than <strong>{archiveThresholdInDays}</strong> days: <strong>{tasksToArchive.length}</strong>
+                </Typography>
+
+                <Typography variant="body1">
+                    Tasks in archive: <strong>{archivedTasks.length}</strong>
                 </Typography>
 
                 <Box>
                     <TextField
-                        label='Archive tasks older than (months)'
-                        size='small'
-                        value={archTimeFrame}
+                        label='Days'
+                        helperText='Archive tasks older than (days)'
+                        type='number'
+                        value={archiveThresholdInDays}
                         onChange={timeFrameChangeHandler}
-                        sx={{ mr: 2 }}
+                        sx={{ mr: 2, width: 260 }}
                     />
 
-                    <Button variant='contained' onClick={archiveTasksHandler}>
+                    <Button
+                        variant='contained'
+                        color='error'
+                        disabled={!tasksToArchive.length || !archiveThresholdInDays.length}
+                        onClick={archiveTasksHandler}>
                         Archive
                     </Button>
                 </Box>
+
+                {showAlert && <Alert severity='success' onClose={() => setShowAlert(false)}>
+                    {`Task older than ${archiveThresholdInDays} successfuly archived`}
+                </Alert>}
+
             </Stack>
         </Paper>
 
